baa3d2e0a22020c0c6947bb95c78414432994331
twothreezero <realtwothreezero@gmail.com>
2025-12-30 17:52:38 +0900

    fix server

--- server/server.js
+++ server/server.js
@@ -175,7 +175,7 @@ app.get("/api/share/:id", (req, res) => {
 
 
 // --------- 서버 실행 ---------
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Backend listening on http://localhost:${PORT}`);
 });
